089fcf1c9deb23e9be0e71c7dd6255bd22346226
Marko Smoljanovic <markosmoljanovicjava@gmail.com>
2020-10-31 14:36:10 +0100

    added getter and setter for field address

--- src/model/User.java
+++ src/model/User.java
@@ -22,6 +22,14 @@ public class User {
 		this.age = age;
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", age=" + age + "]";
